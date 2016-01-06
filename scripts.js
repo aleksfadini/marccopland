f230170297daa0e829b694274e320b56ccbed2e9
aleksfadini <aleksfadini@gmail.com>
2016-01-05 21:46:05 -0500

    added fading quotes from the press, randomized

--- scripts.js
+++ scripts.js
@@ -1,17 +1,49 @@
-var quotes = [
-  "This is a quote.",
-  "So is this.",
-  "And another one.",
-  "My god, it's full of stars!"
-]
-
-function fadeInAndOut(divID, quotes, interval) {
-  setInterval(function () {
-    $(divID).fadeOut('slow', function() {
-      $(this).text(quotes[Math.floor(Math.random() * quotes.length)]);
-      $(this).fadeIn('slow');
-    });
-  }, interval);
+function fisherYatesShuffle(array ){
+    var count = array.length,
+      randomnumber,
+      temp;
+    while( count ){
+        randomnumber = Math.random() * count-- | 0;
+        temp = array[count];
+        array[count] = array[randomnumber];
+        array[randomnumber] = temp;
+    }
 }
 
-fadeInAndOut('#quotes', quotes, 3000);
+$(function() {
+    var index = 0;
+    var numItems = $('#quotes li').length;
+
+    // randomize the elements
+    function randomize() {
+        var quotesArray = $('#quotes li').get();
+        fisherYatesShuffle(quotesArray);
+        // re-insert DOM elements in the new random order
+        for (var i = 0; i < quotesArray.length; i++) {
+            $("#quotes ul").append(quotesArray[i]);
+        }
+    }
+
+    // make sure the index is not past the end of the array
+    // if so, randomize the elements again and reset the index
+    function checkIndex() {
+        if (index >= numItems) {
+            randomize();
+            index = 0;
+        }
+    }
+
+    // process the next item with 6 second delay, then fadeOut, then fadeIn of next element
+    function next() {
+        $("#quotes li").eq(index).delay(5000).fadeOut('slow', function() {
+            ++index;
+            checkIndex();
+            $("#quotes li").eq(index).fadeIn('slow', next);
+        });
+
+    }
+    // randomize the items initially and show the first one after randomization
+    randomize();
+    $('#quotes li').hide().eq(0).show();
+    next();
+});
